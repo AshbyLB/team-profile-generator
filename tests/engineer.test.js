be8829ca729dc316ca136f354fa839bd0d9355d9
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-09-14 16:47:46 -0400

    created engineer test

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,1 +1,19 @@
 const Engineer = require('../lib/engineer');
+
+it('Set gitHub', () => {
+    const testValue = 'AshbyLB';
+    const e = new Engineer("Ashby",4, "me@me.com", testValue);
+    expect(e.gitHub).toBe(testValue);
+});
+
+test("getRole() should return \"Engineer\"", () => {
+    const testValue = "Engineer";
+    const e = new Engineer("Ashby", 12, 'me@me.com', 'AshbyLB');
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("get username from gitHub", () => {
+    const testValue = "AshbyLB";
+    const e = new Engineer("Ashby", 12, 'me@me.com', testValue);
+    expect(e.getGitHub()).toBe(testValue);
+});
