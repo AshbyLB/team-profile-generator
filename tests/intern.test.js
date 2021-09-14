e1fd7803b4b92faf0da138434920659400c290a8
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-09-14 17:17:03 -0400

    added the tests and the js files witht he the constructors extended by the employee file.  exported them to the index.js

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -1,1 +1,19 @@
 const Intern = require('../lib/intern');
+
+it('Set school', () => {
+    const testValue = 'Winthrop';
+    const e = new Intern("Ashby",4, "me@me.com", testValue);
+    expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+    const testValue = "Intern";
+    const e = new Intern("Ashby", 12, 'me@me.com', 'Winthrop');
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("get school from schoolStats()", () => {
+    const testValue = "Winthrop";
+    const e = new Intern("Ashby", 12, 'me@me.com', testValue);
+    expect(e.getschoolStats()).toBe(testValue);
+});
--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,1 +1,19 @@
 const Manager = require('../lib/manager');
+
+it('Set office number', () => {
+    const testValue = '2468';
+    const e = new Manager("Ashby",4, "me@me.com", testValue);
+    expect(e.officeNum).toBe(testValue);
+});
+
+test("getRole() should return \"Manager\"", () => {
+    const testValue = "manager";
+    const e = new Manager("Ashby", 12, 'me@me.com', '2468');
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("get office number from officeNum()", () => {
+    const testValue = "2468";
+    const e = new Manager("Ashby", 12, 'me@me.com', testValue);
+    expect(e.getofficeNum()).toBe(testValue);
+});
